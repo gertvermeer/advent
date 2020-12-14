4890a7fc97ee8cb82e027d8a1ba036f0a718f436
Gert <gert@vermeernet.nl>
2020-12-14 22:08:21 +0100

    day14

--- src/main/java/Day14.java
+++ src/main/java/Day14.java
@@ -1,9 +1,9 @@
-import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
 
 import java.io.ObjectInputValidation;
+import java.math.BigInteger;
 import java.sql.SQLOutput;
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
 
 public class Day14 {
 
@@ -12,60 +12,149 @@ public class Day14 {
     public void go() {
         List<String> input = utils.readfile("input14.txt");
 
-        HashMap<Integer,Long> memory = new HashMap<>();
+        HashMap<Integer, BigInteger> memory = new HashMap<>();
 
 
-        int t=-1;
-        String mask ="";
-        while (t<input.size()-1){
+        int t = -1;
+        String mask = "";
+        while (t < input.size() - 1) {
             t++;
-            if (input.get(t).contains("mask")){
-                mask = input.get(t).replace("mask = ","");
+            if (input.get(t).contains("mask")) {
+                mask = input.get(t).replace("mask = ", "");
                 continue;
             }
 
-            Integer key = Integer.parseInt(input.get(t).replaceAll(".*\\[","").replaceAll("\\].*",""));
+            Integer key = Integer.parseInt(input.get(t).replaceAll(".*\\[", "").replaceAll("\\].*", ""));
 
-            long value = Long.parseLong(input.get(t).replaceAll("mem.* = ",""));
-            String binValue =  "0000000000000000000000000000000000000000000"+ Long.toBinaryString(value);
-            binValue = binValue.substring(binValue.length()-36, binValue.length());
+            long value = Long.parseLong(input.get(t).replaceAll("mem.* = ", ""));
+            String binValue = "0000000000000000000000000000000000000000000" + Long.toBinaryString(value);
+            binValue = binValue.substring(binValue.length() - 36, binValue.length());
 
             int i = 1;
-            for (char c : mask.toCharArray()){
-                if (c !='X') {
-                   // System.out.println(binValue +"v");
-                    binValue = binValue.substring(0, i-1) + String.valueOf(c) + binValue.substring(i, binValue.length());
+            for (char c : mask.toCharArray()) {
+                if (c != 'X') {
+                    // System.out.println(binValue +"v");
+                    binValue = binValue.substring(0, i - 1) + String.valueOf(c) + binValue.substring(i, binValue.length());
                     // System.out.println(binValue + "n");
                 }
                 i++;
             }
 
 
-            int bi = 1;
-            Long res = 0L;
-            for (int tc = binValue.length(); tc>0; tc-- ){
-                res = res + (Long.parseLong(binValue.substring(tc-1, tc) ) * bi);
-                bi = bi *2;
-            }
+            BigInteger res = getBigIntFromBinString(binValue);
             memory.put(key, res);
-
-            if (res < 0){
-                System.out.println(res);
-                System.out.printf(binValue);
-            }
 
             System.out.println(binValue);
 
 
         }
 
-        final long[] sum = {0};
-        memory.forEach((k,v) -> {
-            System.out.println(k + "-" +v);
-            sum[0] = sum[0] + v;
-        });
-        System.out.println("total "+ sum[0]);
+        BigInteger sum = BigInteger.valueOf(0);
+        Iterator it = memory.entrySet().iterator();
+
+        while (it.hasNext()) {
+            Map.Entry pair = (Map.Entry) it.next();
+            System.out.println(pair.getKey() + " - " + pair.getValue());
+            BigInteger a = (BigInteger) pair.getValue();
+            sum = sum.add(a);
+            it.remove();
+        }
+
+
+        System.out.println("total " + sum);
 
     }
 
+    public void go2() {
+
+        List<String> input = utils.readfile("input14.txt");
+
+        HashMap<BigInteger, BigInteger> memory = new HashMap<>();
+
+        BigInteger keyBi = BigInteger.valueOf(0);
+
+        int t = -1;
+        String mask = "";
+        while (t < input.size() - 1) {
+            t++;
+            if (input.get(t).contains("mask")) {
+                mask = input.get(t).replace("mask = ", "");
+                continue;
+            }
+            Long key = Long.valueOf(Integer.parseInt(input.get(t).replaceAll(".*\\[", "").replaceAll("\\].*", "")));
+            keyBi = BigInteger.valueOf(key);
+
+            //long value = Long.parseLong(input.get(t).replaceAll("mem.* = ", ""));
+            String binValue = "0000000000000000000000000000000000000000000" + Long.toBinaryString(key);
+            binValue = binValue.substring(binValue.length() - 36, binValue.length());
+
+            int i = 1;
+            List<Integer> xList = new ArrayList<>();
+            for (char c : mask.toCharArray()) {
+                if (c != 'X') {
+                    if(c!='0'){
+                        binValue = binValue.substring(0, i - 1) + String.valueOf(c) + binValue.substring(i, binValue.length());
+                    }
+                } else {
+                    xList.add(i);
+                }
+                i++;
+            }
+
+
+            generateKeys(memory, binValue, xList, keyBi);
+
+            //System.out.println(binValue);
+
+        }
+
+        BigInteger sum = BigInteger.valueOf(0);
+        Iterator it = memory.entrySet().iterator();
+
+        System.out.println(memory.size());
+
+        while (it.hasNext()) {
+            Map.Entry pair = (Map.Entry) it.next();
+            System.out.println(pair.getKey() + " - " + pair.getValue());
+            BigInteger a = (BigInteger) pair.getValue();
+            sum = sum.add(a);
+            it.remove();
+        }
+
+
+        System.out.println("total " + sum);
+
+    }
+
+    private void generateKeys(HashMap<BigInteger, BigInteger> memory, String binValue, List<Integer> xList, BigInteger res) {
+        BigInteger keyBi = BigInteger.valueOf(0);
+        if (xList.size() == 1){
+            String binValue0 = binValue.substring(0, xList.get(0) - 1) + "0" + binValue.substring(xList.get(0), binValue.length());
+            keyBi = getBigIntFromBinString(binValue0);
+            memory.put(keyBi, res);
+            binValue0 = binValue.substring(0, xList.get(0) - 1) + "1" + binValue.substring(xList.get(0), binValue.length());
+            keyBi = getBigIntFromBinString(binValue0);
+            memory.put(keyBi, res);
+            return;
+        } else {
+            String binValue0 = binValue.substring(0, xList.get(0) - 1) + "0" + binValue.substring(xList.get(0), binValue.length());
+            generateKeys(memory, binValue0, xList.subList(1,xList.size()), res);
+            binValue0 =  binValue.substring(0, xList.get(0) - 1) + "1" + binValue.substring(xList.get(0), binValue.length());
+            generateKeys(memory, binValue0, xList.subList(1,xList.size()), res);
+        }
+
+    }
+
+    private BigInteger getBigIntFromBinString(String binValue) {
+        BigInteger bi = BigInteger.valueOf(1);
+        BigInteger res = BigInteger.valueOf(0);
+        for (int tc = binValue.length(); tc > 0; tc--) {
+            if (binValue.substring(tc - 1, tc).equals("1")) {
+                res = res.add(bi);
+            }
+
+            bi = bi.multiply(BigInteger.valueOf(2));
+        }
+        return res;
+    }
 }
